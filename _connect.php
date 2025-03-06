ae6843a4daf755590efc2ca09341a13379c7d584
PopyPepo <kanaphol.a@gmail.com>
2025-03-06 17:58:47 +0700

    insert AllowDynamicProperties

--- _connect.php
+++ _connect.php
@@ -1,11 +1,13 @@
 <?php
+#[\AllowDynamicProperties]
+class PDOx extends PDO { }
 	try {
 		$servername = "localhost";
 		$database = "example_db";
 		$username = "root";
 		$password = "";
 		$conn_string = 'mysql:host='.$servername.'; dbname='.$database.';charset=utf8';
-		$conn = new PDO($conn_string, $username, $password);
+		$conn = new PDOx($conn_string, $username, $password);
 	} catch (PDOException $e) {
 		//print "Error!: " . $e->getMessage() . "<br/>";
 		echo json_encode($e->getMessage());
